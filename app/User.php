b7323592f18b12463fa5706aeb52562aa388e869
manelgavalda <manelgavalda@iesebre.com>
2016-12-10 00:48:25 +0100

    Using Scool User

--- app/User.php
+++ app/User.php
@@ -2,28 +2,9 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Scool\Foundation\User as ScoolUser;
 
-class User extends Authenticatable
+class User extends ScoolUser
 {
-    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
 }
