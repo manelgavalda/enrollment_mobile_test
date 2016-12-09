3f94e67f75a6d0ac32e2d8d914ad35fe7e4a5902
manelgavalda <manelgavalda@iesebre.com>
2016-12-10 00:53:24 +0100

    My seeder

---
+++ database/seeds/ManelSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        try {
+            factory(User::class)->create([
+                    "name" => "Manel Gavaldà Andreu",
+                    "email" => "manelgavalda@iesebre.com",
+                    "password" => bcrypt(env('MANEL_PASS', 'secret'))]
+            );
+        } catch (\Illuminate\Database\QueryException $exception) {
+        }
+    }
+}
