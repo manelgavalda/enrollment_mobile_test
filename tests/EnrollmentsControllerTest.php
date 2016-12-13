a6f63f8302586e7b13c296a91ba04dccf8011eee
manel <manelgavalda@iesebre.com>
2016-12-13 16:47:03 +0100

    Testing

---
+++ tests/EnrollmentsControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Class EnrollmentsControllerTest
+ */
+class EnrollmentsControllerTest extends TestCase
+{
+    /**
+     *
+     */
+    public function testsIndex()
+    {
+        
+    }
+}
