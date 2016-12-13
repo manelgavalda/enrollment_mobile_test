ef8e8361f6aab41bc05f9495d8ad2b302d6de04b
manel <manelgavalda@iesebre.com>
2016-12-13 17:07:45 +0100

    Testing

--- tests/EnrollmentsControllerTest.php
+++ tests/EnrollmentsControllerTest.php
@@ -9,11 +9,30 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
  */
 class EnrollmentsControllerTest extends TestCase
 {
+
+    use DatabaseMigrations;
     /**
      *
      */
+
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
     public function testsIndex()
     {
-        
+
+//        dd(route('enrollments.index'));
+        $user = factory(App\User::class)->create();
+        $this->actingAs($user);
+        $this->get('enrollments')->dump();
+        $this->assertResponseOk();
     }
 }
