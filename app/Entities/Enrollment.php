460d784544986d310fd8741a83b1061ad3ddede5
manel <manelgavalda@iesebre.com>
2016-12-15 21:19:18 +0100

    Laravel permission

--- app/Entities/Enrollment.php
+++ app/Entities/Enrollment.php
@@ -2,14 +2,15 @@
 
 namespace App\Entities;
 
+use Acacha\Names\Traits\Nameable;
 use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
 class Enrollment extends Model implements Transformable
 {
-    use TransformableTrait;
+    use TransformableTrait,Nameable;
 
-    protected $fillable = [];
-
+    protected $fillable = ['name'];
+com
 }
