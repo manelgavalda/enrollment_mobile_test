c8897c77faecc032f5d93a9df3d93b2bb96dcfad
manelgavalda <manelgavalda@iesebre.com>
2016-12-16 22:26:47 +0100

    index view refractor

--- resources/views/enrollments/index.blade.php
+++ resources/views/enrollments/index.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Enrollment</div>
+                    <div class="panel-heading">Enrollments</div>
                     <div class="panel-body">
                         <table class="table table-bordered">
                             <thead>
@@ -23,16 +23,9 @@
                             @foreach ($enrollments as $enrollment)
                                 <tr>
                                     <td>{{$enrollment->id}}</td>
-                                    <td>{{$enrollment->name}}</td>
                                 </tr>
                             @endforeach
-                            <tr v-for="(todo, index) in filteredTodos">
-                                    <div class="progress progress-xs">
-                                        <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
-                                    </div>
-                                </td>
-                                <td><span class="badge bg-red">55%</span></td>
-                            </tr>
+
                             </tbody>
 
                         </table>
