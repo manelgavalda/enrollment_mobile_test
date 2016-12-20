13381ae71e05d002a0f7c251bc6069eb3ea6dbe3
manel <manelgavalda@iesebre.com>
2016-12-20 18:00:21 +0100

    Danger commented

--- resources/views/enrollments/index.blade.php
+++ resources/views/enrollments/index.blade.php
@@ -26,11 +26,11 @@
                             <h4><i class="icon fa fa-check"></i> Done!</h4>
                             enrollment create ok!
                         </div>
-                        <div class="alert alert-danger alert-dismissible">
-                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-                            <h4><i class="icon fa fa-check"></i> Oops!</h4>
-                            Something is wrong!
-                        </div>
+                        {{--<div class="alert alert-danger alert-dismissible">--}}
+                            {{--<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>--}}
+                            {{--<h4><i class="icon fa fa-check"></i> Oops!</h4>--}}
+                            {{--Something is wrong!--}}
+                        {{--</div>--}}
 
                         <form role="form" action="/enrollments" method="post" id="createEnrollment">
                             <!-- text input -->
