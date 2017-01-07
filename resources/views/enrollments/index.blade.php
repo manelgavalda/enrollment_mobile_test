8826ed244f22d6844ce71351d05d4db2ce5e926d
manelgavalda <manelgavalda@iesebre.com>
2017-01-07 23:30:49 +0100

    Update method error solved

--- resources/views/enrollments/index.blade.php
+++ resources/views/enrollments/index.blade.php
@@ -75,11 +75,74 @@
                             {{--</div>--}}
 
                         </form>
-
                     </div>
+
                     <!-- /.box-body -->
                     <div class="box-footer">
                         <button class="btn btn-primary" onclick="document.getElementById('createEnrollment').submit();" type="submit">Create</button>
+                    </div>
+                </div>
+
+                {{--edit form--}}
+                <div class="box box-default">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Update enrollment</h3>
+
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                            </button>
+
+                        </div>
+                        <!-- /.box-tools -->
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        @if (Session::has('message'))
+                            <div class="alert alert-success alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <h4><i class="icon fa fa-check"></i> Done!</h4>
+                                {{ session('message') }}
+                            </div>
+                        @endif
+                        {{--<div class="alert alert-danger alert-dismissible">--}}
+                        {{--<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>--}}
+                        {{--<h4><i class="icon fa fa-check"></i> Oops!</h4>--}}
+                        {{--Something is wrong!--}}
+                        {{--</div>--}}
+
+                        <form role="form" action="/enrollments/{{$id=1}}" method="post" id="updateEnrollment">
+                            <!-- text input -->
+                            {{ csrf_field() }}
+
+                            <input type="hidden" name="_method" value="PUT">
+
+                            <input type="hidden" name="validated" value="1">
+                            <input type="hidden" name="finished" value="1">
+                            <input type="hidden" name="study_id" value="1">
+                            <input type="hidden" name="course_id" value="1">
+                            <input type="hidden" name="classroom_id" value="1">
+
+                            @php
+                                $warning="";
+                                if ($errors->has('name')) {
+                                    $warning="has-warning";
+                                }
+                            @endphp
+
+                            <div class="form-group {{ $warning }}">
+                                <label class="control-label" for="inputWarning"><i class="fa fa-pencil"></i>Id</label>
+                                <input type="text" class="form-control" id="inputWarning" placeholder="Id" name="id" value="{{old('id')}}">
+                                @foreach ($errors->get('name') as $message)
+                                    <span class="help-block"> {{ $message }}</span>
+                                @endforeach
+                            </div>
+
+                        </form>
+                    </div>
+
+                    <!-- /.box-body -->
+                    <div class="box-footer">
+                        <button class="btn btn-primary" onclick="document.getElementById('updateEnrollment').submit();" type="submit">Update</button>
                     </div>
                 </div>
 
@@ -116,8 +179,9 @@
                                     <td>
                                         <form action="/enrollments/{{$enrollment->id}}" method=POST>
                                             {{ csrf_field() }}
-                                            <button name="_method" value="DELETE" class="btn btn-danger"><i class="fa fa-fw fa-trash"></i></button>
+                                            <button class="btn btn-danger" name="_method" value="DELETE" ><i class="fa fa-fw fa-trash"></i></button>
                                         </form>
+                                            <button class="btn btn-primary" onclick="document.getElementById('updateEnrollment').submit();" type="submit"><i class="fa fa-fw fa-edit"></i></button>
                                     </td>
                                     {{--<td> {{ $enrollment->validated }} </td>--}}
                                     {{--<td> {{ $enrollment->finished }} </td>--}}
