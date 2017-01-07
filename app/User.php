e4c4a1fb0ba73ab7d69948875864e82b57a16452
manelgavalda <manelgavalda@iesebre.com>
2017-01-07 22:19:55 +0100

    Normal user for seeder

--- app/User.php
+++ app/User.php
@@ -2,11 +2,33 @@
 
 namespace App;
 
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
-use Scool\Foundation\User as ScoolUser;
+//use Scool\Foundation\User as ScoolUser;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends ScoolUser
+//class User extends ScoolUser
+/**
+ * Class User
+ * @package App
+ */
+class User extends Authenticatable
 {
-    use HasApiTokens, HasRoles;
+    use HasApiTokens, Notifiable, HasRoles;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password'
+    ];
+
+    /**
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
 }
+
