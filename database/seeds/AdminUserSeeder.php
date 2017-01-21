dbd22f561444cffbfdb889f95dbc5f7ecd54ed53
manelgavalda <manelgavalda@iesebre.com>
2017-01-21 23:08:53 +0100

    Changed order to create roles after user

--- database/seeds/AdminUserSeeder.php
+++ database/seeds/AdminUserSeeder.php
@@ -18,8 +18,8 @@ class AdminUserSeeder extends Seeder
             factory(App\User::class)->create([
                     "name" => "manelgavalda",
                     "email" => "manelgavalda@iesebre.com",
-                    "password" => bcrypt(env('ADMIN_PWD', '123456'))/*->assignRole('administrator')*/]
-            );
+                    "password" => bcrypt(env('ADMIN_PWD', '123456'))]
+            )->assignRole('manage enrollments');
         } catch (\Illuminate\Database\QueryException $exception) {
 
         }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,8 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminUserSeeder::class);
         //Added enrollments seeder.
         $this->call(EnrollmentMobileSeeder::class);
+
+        $this->call(AdminUserSeeder::class);
     }
 }
