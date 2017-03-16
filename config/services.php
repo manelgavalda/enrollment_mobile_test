df4cd96fd974b80adf050855d6f334aab38078dc
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-03-16 18:08:03 +0100

    Added twitter oauth

--- config/services.php
+++ config/services.php
@@ -43,6 +43,12 @@ return [
     */
     #llum_services
 
+    'twitter' => [
+        'client_id' => env('TWITTER_OAUTH_APP_ID'),
+        'client_secret' => env('TWITTER_OAUTH_APP_SECRET'),
+        'redirect' => env('TWITTER_OAUTH_APP_REDIRECT_URL'),
+    ],
+
     'google' => [
         'client_id' => env('GOOGLE_OAUTH_APP_ID'),
         'client_secret' => env('GOOGLE_OAUTH_APP_SECRET'),
