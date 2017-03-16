ab0833a5b6f395b5b1d932213167317ce73c9dd1
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-03-16 16:13:49 +0100

    Added enrollment permission for all users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -81,6 +81,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
-        ]);
+        ])->assignRole("manage enrollments");
     }
 }
