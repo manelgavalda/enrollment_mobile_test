79a9f33a5270121902a3310191a4c9ea783254c9
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-03-22 19:58:07 +0100

    Changing App\ to Manelgavalda\EnrollmentMobileTest

--- database/seeds/AdminUserSeeder.php
+++ database/seeds/AdminUserSeeder.php
@@ -15,7 +15,7 @@ class AdminUserSeeder extends Seeder
     public function run()
     {
         try {
-            factory(App\User::class)->create([
+            factory(Manelgavalda\EnrollmentMobileTest\User::class)->create([
                     "name" => "manel",
                     "email" => "manelgavalda@iesebre.com",
                     "password" => bcrypt(env('ADMIN_PWD', '123456'))]
