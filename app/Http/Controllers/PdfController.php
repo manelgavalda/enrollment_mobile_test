7c4e5dacda9912d46149723b66d66b7ea67afe7b
Manel Gavaldà Andreu <manelgavalda@iesebre.com>
2017-05-16 16:12:39 +0200

    Tinkering dusk

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -2,9 +2,24 @@
 
 namespace Manelgavalda\EnrollmentMobileTest\Http\Controllers;
 
+use App;
 use Illuminate\Http\Request;
 
+/**
+ * Class PdfController
+ * @package Manelgavalda\EnrollmentMobileTest\Http\Controllers
+ */
 class PdfController extends Controller
 {
-    //
+    public function enrollments()
+    {
+        return 'todo';
+    }
+
+    public function enrollment()
+    {
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadHTML('<h1>Hello World</h1>');
+        return $pdf->stream();
+    }
 }
---
+++ tests/Browser/PdfControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class PdfControllerTest extends DuskTestCase
+{
+    /**
+     * Test enrollments are converted to pdf correctly.
+     *
+     * @return void
+     */
+    public function test_users_are_converted_to_pdf_correctly()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/enrollments/pdf')
+                    ->assertSee('todo');
+        });
+    }
+
+    /**
+     * Test user is converted to pdf correctly.
+     *
+     * @return void
+     */
+    public function test_user_is_converted_to_pdf_correctly()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/enrollment/pdf/1')
+                ->assertSee('Laravel');
+        });
+    }
+}
