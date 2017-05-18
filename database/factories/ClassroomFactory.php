7de35d4d531745e95e8568bc8e404741b115b03d
manelgavalda <manelgavalda@iesebre.com>
2017-05-18 23:50:45 +0200

    Factory relationship ok

--- database/factories/ClassroomFactory.php
+++ database/factories/ClassroomFactory.php
@@ -4,7 +4,6 @@
 $factory->define(Scool\EnrollmentMobile\Models\Classroom::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->word,
-        'enrollment_id' => $faker->randomDigit
-
+        'enrollment_id' => $faker->unique()->numberBetween($min = 1, $max = 20)
     ];
 });
