4a003f22c67bec10e1c20db0689443f6fe698b22
manelgavalda <manelgavalda@iesebre.com>
2017-06-04 22:01:14 +0200

    Working on enrollment wizard

--- database/factories/SubmoduleFactory.php
+++ database/factories/SubmoduleFactory.php
@@ -6,7 +6,8 @@ $factory->define(Scool\EnrollmentMobile\Models\Submodule::class, function (Faker
         'name' => $faker->word,
         'total_hours' => $faker->numberBetween(10, 50),
         'week_hours' => $faker->numberBetween(1, 6),
+        'module_id' => 1,
         'start_date' => $faker->date(),
-        'finish_date' => $faker->date(),
+        'end_date' => $faker->date(),
     ];
 });
