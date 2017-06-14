f64f17382abd314a7d31ad7804bed7959faa170e
manelgavalda <manelgavalda@iesebre.com>
2017-06-14 21:35:22 +0200

    Laravel dusk working

--- tests/Browser/PdfControllerTest.php
+++ tests/Browser/PdfControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Browser;
 
+use Manelgavalda\EnrollmentMobileTest\User;
 use Scool\EnrollmentMobile\Models\Enrollment;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
@@ -59,33 +60,77 @@ class PdfControllerTest extends DuskTestCase
      * Test user is converted to pdf correctly.
      * @return void
      */
-    public function test_users_are_shown_correctly()
+//    public function test_users_are_shown_correctly()
+//    {
+//
+//        $enrollments = $this->createEnrollments(25);
+//
+//        dump($enrollments[0]->name);
+//        $this->browse(function (Browser $browser) use ($enrollments){
+//            $browser->visit('enrollmentspdf/view')
+//            ->assertTitle('Enrollments List')
+//            ->assertSee($enrollments[0]->name);
+//
+//            // Funciona amb css selectors.
+//            $this->assertEquals(3,count($browser->elements('div#enrollments-list table#enrollments-tablelist tr th')));
+//            $this->assertEquals(26,count($browser->elements('div#enrollments-list table#enrollments-tablelist tr')));
+//        });
+//    }
+
+public function test_can_view_login()
+{
+    $this->browse(function (Browser $browser) {
+        $browser->visit('/login')
+//                ->pause(50000)
+            ->assertPathIs('/login');
+//            ->assertSee(' Sign in to start your session ');
+    });
+}
+
+    /**
+     * Test cant view dashboard without login.
+     * @return void
+     * @group failing
+     */
+    public function test_can_view_dashboard_without_login()
     {
 
-        $enrollments = $this->createEnrollments(25);
-
-        dump($enrollments[0]->name);
-        $this->browse(function (Browser $browser) use ($enrollments){
-            $browser->visit('enrollmentspdf/view')
-            ->assertTitle('Enrollments List')
-            ->assertSee($enrollments[0]->name);
-
-            // Funciona amb css selectors.
-            $this->assertEquals(3,count($browser->elements('div#enrollments-list table#enrollments-tablelist tr th')));
-            $this->assertEquals(26,count($browser->elements('div#enrollments-list table#enrollments-tablelist tr')));
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/home')
+//                ->pause(50000)
+                ->assertPathIs('/login');
         });
     }
 
     /**
-     * Test user is converted to pdf correctly.
+     * Test can view dashboard with login.
      * @return void
      * @group failing
      */
-    public function test_show_dashboard_elements_without_authentication()
+    public function test_can_view_dashboard_when_login()
     {
-        $this->browse(function (Browser $browser) {
+        $user = factory(User::class)->create([
+            'name' => 'Manel Gavaldà',
+            'email' => 'dusklogin@iesebre.com'
+        ]);
+
+        $this->browse(function (Browser $browser)  use ($user) {
             $browser->visit('/home')
-                ->assertSee('todo');
+                ->assertPathIs('/login')
+                ->pause(10000)
+                ->type('email', $user->email)
+                ->pause(10000)
+                ->type('password', 'secret')
+                ->pause(10000)
+                ->press('Sign In')
+                ->pause(10000)
+                ->assertPathIs('/home')
+                ->pause(10000)
+                ->assertSee('Dashboard')
+                ->pause(10000)
+                ->assertSee('Enrollment')
+                ->pause(10000)
+                ->assertSee('Manel Gavaldà');
         });
     }
 
