32704c309359234205ab2610e8ebea7f7e2eb135
manelgavalda <manelgavalda@iesebre.com>
2017-06-15 16:02:33 +0200

    basic manifest and reference

--- resources/views/vendor/adminlte/layouts/landing.blade.php
+++ resources/views/vendor/adminlte/layouts/landing.blade.php
@@ -29,6 +29,9 @@ Landing page based on Pratt: http://blacktie.co/demo/pratt/
     <link href="{{ asset('/css/all.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
 
+    <link rel="manifest" href="/manifest.json">
+
+
     <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
 
