9d8c94503bc35e81f74f7a9e0677205fc1e1661f
manelgavalda <manelgavalda@iesebre.com>
2017-06-18 21:48:16 +0200

    unaunthenticated

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,5 @@ Route::group(['prefix' => 'v1', 'middleware' => ['auth:api']], function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::post('/user/gcmtoken', 'GcmTokensController@addToken');
 });
 
